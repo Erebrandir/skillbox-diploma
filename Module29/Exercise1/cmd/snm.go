898b28e52e3405bb006e0df0179d7e2b4cd91619
Erebrandir <erebrandir@yandex.ru>
2022-03-13 18:48:18 +0300

    Exercise 29.1 - Second Solution Done

---
+++ Module29/Exercise1/cmd/snm.go
@@ -0,0 +1,56 @@
+/*
+Что нужно сделать
+Реализуйте паттерн-конвейер:
+Программа принимает числа из стандартного ввода в бесконечном цикле и передаёт число в горутину.
+Квадрат: горутина высчитывает квадрат этого числа и передаёт в следующую горутину.
+Произведение: следующая горутина умножает квадрат числа на 2.
+При вводе «стоп» выполнение программы останавливается.
+
+Советы и рекомендации
+Воспользуйтесь небуферизированными каналами и waitgroup.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		fmt.Println("Введите число или кодовое слово - стоп: ")
+		var number int
+		fmt.Scan(&number)
+
+		if string(number) != "стоп" {
+			fc := squareOfnNumber(number)
+			sc := multiplicationOfNumber(fc)
+
+			fmt.Println(<-sc)
+			fmt.Println()
+		} else {
+			fmt.Println("Вы ввели стоп слово, работа программы остановлена.")
+			break
+		}
+	}
+}
+
+func squareOfnNumber(num int) chan int {
+	firstChan := make(chan int)
+	go func() {
+		res := num * num
+		firstChan <- res
+	}()
+	close(firstChan)
+	return firstChan
+}
+
+func multiplicationOfNumber(firstChan chan int) chan int {
+	secondChan := make(chan int)
+	fmt.Println(<-firstChan)
+	go func() {
+		num := <-firstChan
+		numNew := 2 * num
+		secondChan <- numNew
+	}()
+	close(secondChan)
+	return secondChan
+}
